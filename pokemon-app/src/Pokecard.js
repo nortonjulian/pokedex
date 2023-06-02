071c1068a693a8bf102ebeb9918dfab0d16c0d3b
Julian <nortonjulian@gmail.com>
2023-06-02 14:00:03 -0600

    second commit

--- pokemon-app/src/Pokecard.js
+++ pokemon-app/src/Pokecard.js
@@ -4,14 +4,14 @@ import './Pokecard.css'
 const poke_id = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 function Pokecard(props) {
-    let imgSrc = `${poke_id}${this.props.id}.png`
+    let imgSrc = `${poke_id}${props.id}.png`
 
     return (
         <div className="Pokecard">
-           <div className="Pokecard-title">{ this.props.name }</div>
+           <div className="Pokecard-title">{ props.name }</div>
            <img className="Pokecard-image">src={imgSrc }</img>
-           <div className="Pokecard-data">{ this.props.type }</div>
-           <div className="Pokecard-data">{ this.props.exp}</div>
+           <div className="Pokecard-data">{ props.type }</div>
+           <div className="Pokecard-data">{ props.exp}</div>
         </div>
     )
   }
--- pokemon-app/src/Pokedex.js
+++ pokemon-app/src/Pokedex.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Pokecard from './Pokecard'
-import './Pokedex.css';
+import './pokedex.css';
 
 function Pokedex(props) {
        let winMessage = null
@@ -12,7 +12,7 @@ function Pokedex(props) {
         <div className="Pokedex">
            <h2 className="Pokedex-title">Pokedex</h2>
            <div className="Pokedex-cards">
-             {props.pokemon(p =>
+             {props.pokemon.map(p =>
               <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience}/>
             )}
            </div>
